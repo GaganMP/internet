88f8777ed03db318982ae6a8153464f4bc658981
GaganMP <145898445+GaganMP@users.noreply.github.com>
2023-10-11 21:39:58 +0530

    initial commit
    internet availability test

---
+++ script.js
@@ -0,0 +1,24 @@
+let message =document.getElementById("message");
+let messageOnline=()=>{
+    message.textContent="Internet Connection Available";
+    message.style.cssText="background-color:#e7f6d5;color:#689f38";
+    alert("you are online");
+};
+
+let messageOffline=()=>{
+    message.textContent="Internet Connection not Available";
+    message.style.cssText="background-color:#ffdde0";
+    alert("you are offline");
+    message.style.cssText="background-color:#ffdde0;color:#d32f2f";
+};
+
+if(window.navigator.onLine)
+{
+    messageOnline();
+}
+else{
+    messageOffline();
+}
+
+window.addEventListener("online",messageOnline);
+window.addEventListener("offline",messageffline);
